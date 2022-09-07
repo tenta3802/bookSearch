74149e64831365fd6f2b4aa1f59f18b6f30620ee
tenta <eunoia3802@gmail.com>
2022-09-07 23:50:44 +0900

    초기 환경 설정 완료

---
+++ src/main/java/eunoia/bookSerch/BookSerchApplication.java
@@ -0,0 +1,13 @@
+package eunoia.bookSerch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSerchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSerchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eunoia/bookSerch/BookSerchApplicationTests.java
@@ -0,0 +1,13 @@
+package eunoia.bookSerch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSerchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
